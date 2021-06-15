8caf5c9dcb39bba194f4c8b15c9134b99e24be03
luiscaputo <luiscaputo15@gmail.com>
2021-06-16 00:21:52 +0100

    :construction: layout-file-register

---
+++ pages/cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/bootstrap.css">
+  <title>login</title>
+</head>
+<body style="background-color:#30363d;">
+  <div class="container mt-3 p-3">
+    <div class="row">
+      <div class="col-sm">
+
+      </div>
+      <div class="col-sm">
+      <h4 style="color: white;">Sistema de Login - Prof.Pazito</h4><br>
+        <form action="" method="post" class="">
+              <input type="text" class="form-control" id="user_name" name="user_name" placeholder="Nome de usuário"><br>
+              <input type="email" class="form-control" id="email" name="email" placeholder="email de usuário"><br>
+              <input type="password" class="form-control" id="password" name="password" placeholder="Palavra Chave"><br>
+          <button class="btn btn-success form-control">Cadastrar</button><br>
+          <a href="../index.php">Se já tem conta, clica aqui!</a>
+        </form>
+      </div>
+      <div class="col-sm">
+      </div>
+    </div>
+  </div>
+</body>
+</html>
