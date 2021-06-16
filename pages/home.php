fbf622d688d506400c1341757bd2a576a3c75e79
luiscaputo <luiscaputo15@gmail.com>
2021-06-16 09:29:23 +0100

    :zap: home-file-start

---
+++ pages/home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/bootstrap.css">
+  <title>Home Page</title>
+</head>
+<body style="background-color:#30363d;">
+  <div class="container mt-3 p-3">
+    <div class="row">
+      <div class="col-sm">
+
+      </div>
+      <div class="col-sm">
+      <h4 style="color: white;">Welcome to home page</h4><br>
+        
+      </div>
+      <div class="col-sm">
+      </div>
+    </div>
+  </div>
+</body>
+</html>
